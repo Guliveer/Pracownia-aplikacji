03ea428e670001358c35c94a81227433e6901d70
Guliveer <none>
2021-11-15 15:10:01 +0100

    15.11.2021, 15:10:01,39 (Updated using script)

--- github-push.cmd
+++ github-push.cmd
@@ -5,7 +5,10 @@ git add ./
 
 git remote add origin https://github.com/Guliveer/Pracownia-aplikacji.git
 
-git commit -m "`date` (Updated using script)"
+set date = date /t
+set time = time /t
+
+git commit -m "%date%, %time% (Updated using script)"
 git branch -M main
 git push -u origin main
 
