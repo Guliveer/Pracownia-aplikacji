9a0f16dd9a021db1693a2296f037c33152a2230e
Guliveer <oliwer.pawelski@gmail.com>
2022-02-08 11:49:20 +0100

    Last update: 08.02.2022 @ 11:49:19,84

---
+++ JS/05. Zmienne/SSU/SSU.js
@@ -0,0 +1,13 @@
+var imie = "Jan";
+var nazwisko = "Kowalski";
+var wiek = 21;
+var zawod = "student";
+
+var nazwa = imie + " " + nazwisko;
+var wiek_t = wiek + " lat";
+
+document.write("<center>");
+document.write("<h1>" + nazwa + "</h1>");
+document.write("<i>" + wiek_t + "</i><br>");
+document.write("<code>" + zawod + "</code>");
+document.write("</center >");
