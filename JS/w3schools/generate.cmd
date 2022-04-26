dfd0ab9ff36ad7abfb19586b89a9c28641b70cb7
Guliveer <oliwer.pawelski@gmail.com>
2022-04-26 12:57:44 +0200

    w3schools (24-29)

--- JS/w3schools/generate.cmd
+++ JS/w3schools/generate.cmd
@@ -10,21 +10,23 @@ set /p "count=>"
 cls
 
 mkdir "%nazwa%"
-echo 2> "%nazwa%/lista.txt"
-cls
+echo. 1> "%nazwa%/lista.txt"
+echo lista.txt has been created
+echo.
 
 setlocal EnableDelayedExpansion
 for /L %%i in (1, 1, %count%) do (
      set "formattedValue=000000%%i"
      
      mkdir "%nazwa%/!formattedValue:~-2!"
-     echo !formattedValue:~-2! has been created
+     echo Folder !formattedValue:~-2! has been created
      
-     echo. 2> "%nazwa%/!formattedValue:~-2!/index.html"
+     echo. 1> "%nazwa%/!formattedValue:~-2!/index.html"
      echo !formattedValue:~-2!/index.html has been created
 
      echo !formattedValue:~-2!. >> "%nazwa%/lista.txt"
      echo. >> "%nazwa%/lista.txt"
+     echo Added line !formattedValue:~-2!
 
      echo.
 )
